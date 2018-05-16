76f488a8f534ec46846518c552733e1bebb82129
aochuang <aochuang@DESKTOP-CHCRUG4>
2018-05-16 20:39:13 +0800

    1

---
+++ EncryptString/EncryptString/EncryptionDecryption.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptString
+{
+    public interface IBindesh
+    {
+        string encode(string str);
+        string decode(string str);
+    }
+
+    public class EncryptionDecryption : IBindesh
+    {
+        public string encode(string str)
+        {
+            string htext = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                htext = htext + (char) (str[i] + 10 - 1 * 2);
+            }
+
+            return htext;
+        }
+
+        public string decode(string str)
+        {
+            string dtext = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                dtext = dtext + (char) (str[i] - 10 + 1 * 2);
+            }
+
+            return dtext;
+        }
+    }
+}
